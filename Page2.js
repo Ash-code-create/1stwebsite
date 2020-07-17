78e18372175928a483671ae5f1046ca08b2c0f71
Ash-code-create <68404398+Ash-code-create@users.noreply.github.com>
2020-07-17 10:48:10 -0400

    Update Page2.js

--- Page2.js
+++ Page2.js
@@ -1,1 +1,17 @@
+/* global createCanvas, background*/
+var r, g, b, randomSize;
 
+function setup(){
+  createCanvas(800, 500);
+  background(0, 0, 0);
+}
+
+function draw(){
+  noStroke();
+  fill(r, g, b);
+  r = random(0, 256);
+  g = random(0, 256);
+  b = random(0, 256);
+  ellipse(mouseX, mouseY, randomSize); 
+  randomSize = random(1, 101);
+}
