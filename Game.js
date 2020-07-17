65a298c0995d364abae999d756bf8d2183084e18
Ash-code-create <68404398+Ash-code-create@users.noreply.github.com>
2020-07-17 13:46:09 -0400

    Update Game.js

--- Game.js
+++ Game.js
@@ -1,12 +1,10 @@
 /*global createCanvas, background, rect, fill, triangle, noStroke, width, ellipse, line, stroke, strokeWeight*/
 
 var position;
-var img;
 
 function setup() {
   createCanvas(800, 500);
   background(200, 200, 200);
-  img = loadimage("gamebolly.jpeg");
   position = 0;
 }
 
@@ -18,7 +16,6 @@ function draw() {
   //rect(x top L, y top L, width, height)
   fill(11, 132, 163);
   rect(0, 400, width, 350);
-  image(img 0,0);
 
   stroke(11, 132, 163);
   strokeWeight(5);
